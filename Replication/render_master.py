5831c5939bd2a45305db3dc9a13c44335f4d5823
Gavin Parker <gp14958@my.bristol.ac.uk>
2018-02-14 15:35:35 +0000

    Got master slave mostly working

---
+++ Replication/render_master.py
@@ -0,0 +1,37 @@
+from subprocess import Popen, PIPE
+import os
+
+
+class Master:
+    worker = None
+
+    def __init__(self, blender_path):
+        self.blender_path = blender_path
+
+    def start_worker(self, scene):
+        myargs = [
+            self.blender_path,
+            scene,
+            "--background",
+            "--python",
+            "render_worker.py"
+        ]
+        self.worker = Popen(myargs, stdin=PIPE)
+
+    def start_render(self, envmap):
+        self.worker.stdin.write("{}\n".format(envmap))
+
+    def stop_worker(self):
+        self.worker.stdin.write('stop\n')
+
+
+def test():
+    master = Master('/home/gavin/blender-2.79-linux-glibc219-x86_64/blender')
+    master.start_worker('test_elephant.blend')
+    for filename in os.listdir('envmaps'):
+        print(filename)
+        master.start_render(filename)
+    master.stop_worker()
+
+if __name__ == "__main__":
+    test()
--- Replication/render_worker.py
+++ Replication/render_worker.py
@@ -1,19 +1,32 @@
 import bpy
+import sys
+import os
 
 """Class to handle rendering of test scenes in Blender via subprocess"""
+
+
 class RenderWorker:
     def __init__(self):
         bpy.context.scene.render.engine = 'CYCLES'
 
-    def render_image(self, name):
+    def render_image(self, name, envmap):
+        print(os.listdir("{}/envmaps".format(os.getcwd())))
+        bpy.data.images.load('{}/envmaps/{}'.format(os.getcwd(),envmap), check_existing=False)
+        bpy.data.scenes['Scene'].world.node_tree.nodes['Environment Texture'].image = bpy.data.images[envmap]
         bpy.data.scenes['Scene'].render.filepath = name
         bpy.ops.render.render(write_still=True)
 
 
-
 def main():
     worker = RenderWorker()
-    worker.render_image('test.png')
+    count = 0
+    for command in sys.stdin:
+        if '.hdr' in command:
+            worker.render_image('{}.png'.format(count), command)
+            count += 1
+        if 'stop' in command:
+            return
+
 
 if __name__ == "__main__":
     main()
